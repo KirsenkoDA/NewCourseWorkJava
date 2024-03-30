ccfed12a53699ac5a6b37d14f7aa2998cdbb00d6
KirsenkoDA <new.helper@mail.ru>
2024-03-30 16:49:09 +0300

    added working crud operations, added layout(not working)

---
+++ src/main/java/ru/vlsu/ispi/dto/UserUpdateDTO.java
@@ -0,0 +1,62 @@
+package ru.vlsu.ispi.dto;
+
+import ru.vlsu.ispi.models.Role;
+
+import java.util.Set;
+
+public class UserUpdateDTO {
+    Long id;
+    String name;
+    String email;
+    String phoneNumber;
+    Set<Role> roles;
+    boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
